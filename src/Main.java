6e05146f67173d077d948a1cee3d922f2e4818d2
MikaelaMathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-08-23 10:11:04 +0200

    Menu

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Elpriser");
+        System.out.println("========");
+        System.out.println("1. Inmatning");
+        System.out.println("2. Min, Max och Medel");
+        System.out.println("3. Sortera");
+        System.out.println("4. Bästa Laddningstid (4h)");
+        System.out.println("e. Avsluta");
+
+    }
+}
