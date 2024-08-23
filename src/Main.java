598d5391b56916e103d803933d99af2cbb4e144a
MikaelaMathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-08-23 11:10:33 +0200

    Adding input from users to elpriser per hour

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
+        int[] elPriser = new int[24];
 
         while (!"e".equalsIgnoreCase(input)) {
             System.out.println("Elpriser");
@@ -17,10 +18,27 @@ public class Main {
             if ("1".equals(input)) {
                 //Do option 1 - Inmatning
                 System.out.println("Du har valt: " + input);
-
+                //Input av elpriser för varje timme
+                System.out.println("Ange elpriser i öre per kwh för varje timme under dygnet");
+                for (int i = 0; i < 24; i++) {
+                    while (true) {
+                        System.out.print(String.format("Ange pris för timme %02d-%02d: ", i, (i + 1) % 24));
+                        String prisInput = scanner.nextLine();
+                        try {
+                            elPriser[i] = Integer.parseInt(prisInput);  // Försök att konvertera input till ett heltal
+                            break;  // Om inmatningen lyckas, bryt loopen och gå till nästa timme
+                        } catch (NumberFormatException e) {
+                            System.out.println("Felaktig inmatning! Ange ett giltigt heltal för priset i öre.");
+                        }
+                    }
+                }
+                System.out.println("Priserna har registrerats.");
             } else if ("2".equals(input)) {
-                //Do option 2
+                //Do option 2 - Min, Max och Medel
                 System.out.println("Du har valt: "+ input);
+                if (elPriser[0] == 0) {
+                    System.out.println("Inga elpriser är registrerade");
+                }
             }
             else if ("3".equals(input)) {
                 //Do option 3
